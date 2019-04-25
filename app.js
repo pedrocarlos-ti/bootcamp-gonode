48dbf6510c0b361324351855df670459f8c2f991
pedrocarlos-ti <pedrocarlos.ti@gmail.com>
2019-04-25 20:35:02 -0300

    fix: removed old files

--- app.js
+++ app.js
@@ -1,43 +1,18 @@
-const express = require("express");
-const nunjucks = require("nunjucks");
-const app = express();
+const express = require('express')
+const nunjucks = require('nunjucks')
+const app = express()
 
-nunjucks.configure("views", {
+nunjucks.configure('views', {
   autoescape: true,
   express: app,
   watch: true
-});
+})
 
-app.use(express.urlencoded({ extended: false }));
-app.set("view engine", "njk");
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
 
-const users = ["Pedro", "Carlos", "Gomes", "Santos"];
+app.get('/', (req, res) => {
+  res.render('welcome')
+})
 
-// Modelo de QueryParams
-app.get("/", (req, res) => {
-  console.log(`Bem vindo ${req.query.name}`);
-  res.json({
-    message: `Bem vindo ${req.query.name}`
-  });
-});
-
-// Modelo de Params
-app.get("/login/:name", (req, res) => {
-  console.log(`Bem vindo ${req.params.name}`);
-  res.send();
-});
-
-app.get("/nunjucks", (req, res) => {
-  return res.render("list", { users });
-});
-
-app.get("/new", (req, res) => {
-  return res.render("new");
-});
-
-app.post("/create", (req, res) => {
-  users.push(req.body.user);
-  res.redirect("/nunjucks");
-});
-
-app.listen(3000, console.log("Server rodando.."));
+app.listen(3000)
