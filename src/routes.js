d6b76a639abfa51e33c4ee02e4071fac4fc21f34
pedrocarlos-ti <pedrocarlos.ti@gmail.com>
2019-05-02 02:45:57 -0300

    add: pdf generator | router pdf

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const routes = express.Router()
+const nunjucks = require('nunjucks')
+const pdf = require('html-pdf')
 
-routes.get('/', (req, res) => res.render('auth/signup'))
+routes.get('/', (req, res) => {
+  res.render('auth/signup', {
+    msg: 'Hello world!!'
+  })
+})
+
+routes.get('/pdf', (req, res) => {
+  const renderedHtml = nunjucks.render('auth/signup.njk', {
+    msg: 'Hello world 2!!'
+  })
+  pdf.create(renderedHtml).toStream((err, stream) => {
+    if (err) console.log(err)
+    stream.pipe(res)
+  })
+})
 
 module.exports = routes
